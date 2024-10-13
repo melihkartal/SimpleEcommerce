f4dfe6b1c163b1aceabd2e1a643d95873287cd35
melih <86454129+melihkartal@users.noreply.github.com>
2024-10-13 12:25:31 -0400

    created an mvc application

---
+++ simpleEcommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace simpleEcommerce.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
